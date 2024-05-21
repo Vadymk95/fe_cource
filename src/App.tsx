422ee94d7ea90a2795ccf1178b5e54d39cf807dc
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-05-21 23:35:22 +0300

    webpack 7/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { FC, Suspense } from 'react';
-import './index.scss';
 import { Link, Route, Routes } from 'react-router-dom';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 export const App: FC = () => {
+  const { theme, toggleTheme } = useTheme();
+  const handleToggleTheme = () => toggleTheme(theme);
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={handleToggleTheme}>Theme</button>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
       <Suspense fallback={<div>Loading...</div>}>

63fabbef1e85109e5c080749e48c86db3dee2762
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-05-21 20:08:03 +0300

    webpack 6/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
-import { FC } from 'react';
+import { FC, Suspense } from 'react';
 import './index.scss';
+import { Link, Route, Routes } from 'react-router-dom';
+import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 
 export const App: FC = () => {
-  return <div className="app">App</div>;
+  return (
+    <div className="app">
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };

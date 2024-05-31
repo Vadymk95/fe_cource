4ff10ed7dad873192ac070f2f69a3382882f5005
Vadym Kononenko <70842045+Vadymk95@users.noreply.github.com>
2024-05-31 17:16:20 +0300

    app config 11/12

---
+++ src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { i18nChunks } from 'shared/constants';
+import { classNames } from 'shared/lib';
+import cls from './NotFoundPage.module.scss';
+
+interface NotFoundPageProps {
+    className?: string;
+}
+
+const NotFoundPage: FC<NotFoundPageProps> = ({ className = '' }) => {
+    const NotFoundPageClassList = classNames(cls.NotFoundPage, {}, [className]);
+    const { t } = useTranslation(i18nChunks.NOT_FOUND_PAGE);
+
+    return (
+        <div className={NotFoundPageClassList}>{t('Страница не найдена')}</div>
+    );
+};
+
+export default NotFoundPage;
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,4 +1,5 @@
 export enum i18nChunks {
     ABOUT_PAGE = 'about',
     MAIN_PAGE = 'main',
+    NOT_FOUND_PAGE = 'notFound',
 }
